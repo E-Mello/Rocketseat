dcd4f4d6b9a7c7efdb4dea1a750738ec00c3f8a9
E-Mello <e.mello.dev@gmail.com>
2022-02-09 17:32:05 -0400

    Eleventh Commit

--- Js-Study/exercicios04.js
+++ Js-Study/exercicios04.js
@@ -1,7 +1,7 @@
 /*
     Buscando e contando dados em Arrays
     
-    Baseado no Array de Livros por Categoría abaixo, faaça os
+    Baseado no Array de Livros por Categoría abaixo, faça os
     seguintes desafios:
 
         - Contar o número de categorías e o número de livros em cada
@@ -11,3 +11,17 @@
         - Trannsformar a função acima em uma função que irá receber
         o nome do autor e devolver os livros desse autor.
 */
+
+const booksByCategory = [{
+        category: "Riqueza",
+        books: [
+
+        ],
+    },
+    {
+        category: "Inteligência Emocional",
+        books: [
+
+        ],
+    },
+];
