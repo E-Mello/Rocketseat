bb428965f0254f7a122acd71536c1ab20056d492
mello <e.mello.dev@gmail.com>
2022-02-26 12:01:56 -0400

    End NodeJS Class

--- NodeJS-Study/events.js
+++ NodeJS-Study/events.js
@@ -4,4 +4,13 @@
 const { EventEmitter } = require('events')
     // console.log(EventEmitter);
 const ev = new EventEmitter()
-console.log(ev);
+    // console.log(ev);
+
+// ev.on('saySomething', (message) => {
+//     console.log('Eu ouvi voce', message);
+// })
+ev.once('saySomething', (message) => {
+    console.log('Eu ouvi voce', message);
+})
+ev.emit('saySomething', "Mello")
+ev.emit('saySomething', "Jubileu")
