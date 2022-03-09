2b7cfad66154ae469be783b290d7dc1701bbe3e3
root <root@pop-os.localdomain>
2022-03-09 17:51:43 -0400

    First Class Ignite

---
+++ Ignite/01-github-explorer/src/App.jsx
@@ -0,0 +1,3 @@
+export function App(){
+    return <h1>Hello World! v2.0</h1>
+}
---
+++ Ignite/01-github-explorer/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    devServer: {
+        static: {
+          directory: path.resolve(__dirname, "public"),
+          staticOptions: {},
+          // Don't be confused with `devMiddleware.publicPath`, it is `publicPath` for static directory
+          // Can be:
+          // publicPath: ['/static-public-path-one/', '/static-public-path-two/'],
+          publicPath: "/static-public-path/",
+          // Can be:
+          // serveIndex: {} (options for the `serveIndex` option you can find https://github.com/expressjs/serve-index)
+          serveIndex: true,
+          // Can be:
+          // watch: {} (options for the `watch` option you can find https://github.com/paulmillr/chokidar)
+          watch: true,
+        },
+      },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            }
+        ],
+    }
+};
