f5834c56724766cc488585ceb361d5180489012a
Edio Mello <e.mello.dev@gmail.com>
2022-06-27 23:14:44 -0400

    Second class

--- Ignite/Aulas/01-Fundamentos-ReactJS/src/App.jsx
+++ Ignite/Aulas/01-Fundamentos-ReactJS/src/App.jsx
@@ -1,8 +1,15 @@
 // JSX = JavaScript + XML
 import {Post} from './Post';
+import { Header} from './components/Header';
+import './styles.css'
+
+
+
 export function App() {
     return (
       <div>
+        <Header />
+
         <Post  
           author="Edio Mello"
           content="Lorem ipsum dolor sit amet, consectetur adipiscing el"
---
+++ Ignite/Aulas/01-Fundamentos-ReactJS/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import styles from './Header.module.css';
+
+export function Header() {
+    return (
+        <header className="header">
+        <strong>Ignite Feed</strong>
+        </header>
+    );
+}
