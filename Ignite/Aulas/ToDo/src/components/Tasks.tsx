f1e29008da1cd46d216932c7dd337bd036308b5e
Edio Mello <e.mello.dev@gmail.com>
2022-07-13 20:30:01 -0400

    Add Content

--- Ignite/Aulas/ToDo/src/components/Tasks.tsx
+++ Ignite/Aulas/ToDo/src/components/Tasks.tsx
@@ -1,7 +1,18 @@
+import styles from './Tasks.module.css';
+
+import clipboard from '../assets/Clipboard.svg'
+
 export function Tasks() {
     return (
-        <div>
-            
+        <div className={styles.tasks}>
+            <div className={styles.info}>
+                <input></input>
+                <button></button>
+            </div>
+            <div className={styles.list}>
+                <img src={clipboard}></img>
+                <input type="text" name="" id="" />
+            </div>
         </div>
     )
 }
