09401e583d209dca5724fde7719242e94b285adc
Mello <e.mello.dev@gmail.com>
2022-08-30 18:21:54 -0400

    Add content ToDo

--- Ignite/Aulas/ToDo/src/components/TaskList.tsx
+++ Ignite/Aulas/ToDo/src/components/TaskList.tsx
@@ -4,42 +4,49 @@ import clipboard from '../assets/Clipboard.svg'
 
 import { v4 as uuid } from 'uuid';
 import { ChangeEvent, FormEvent, useEffect, useState, InvalidEvent } from 'react';
-import { PlusCircle } from 'phosphor-react';
-import { Task } from './Task';
+import { FlagCheckered, PlusCircle, Trash } from 'phosphor-react';
+import is from 'date-fns/esm/locale/is/index.js';
+export interface TaskItem {
+    //npm install uuidv4 (generate unid id)  
+    id: number ;
+    content: string;
+    isComplete: boolean;
+}
 
 export function TaskList() {
 
     const [newTask, setNewTask] = useState('');
-    const [tasks, setTasks] = useState([
-        'Write here'
-    ])
-    const [count, setCount] = useState(0);
-    
-    const todos = [
-        {
-            id: 1,
-            content: 'teste',
-            isComplete: true,
-        },
-        {
-            id: 2,
-            content: 'teste',
-            isComplete: false,
-        }
-    ]
+    const [tasks, setTasks] = useState<TaskItem[]>([]);
+    const [isChecked, setIsChecked] = useState(false);
+    const [qtdMarked, setQtdMarked] = useState<TaskItem[]>([]);
 
+
+    useEffect(() => {
+        let bdLocal = localStorage.getItem('tasks');
+
+        if (bdLocal) {
+            let obj = JSON.parse(bdLocal);
+            setTasks(obj);
+        }
+    }, [])
+
+    useEffect(() => {
+        if (tasks.length > 0) {
+
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+        }
+    }, [tasks]);
 
     function handleNewTask(event: FormEvent) {
         event.preventDefault();
 
-        setTasks([...tasks, newTask]);
+        const task: TaskItem = {
+            content: newTask,
+            id: Math.random() * 1000,
+            isComplete: false,
+        }
+        setTasks([...tasks, task]);
         setNewTask('');
-
-        setCount((state) => {
-            return state + 1;
-        });
-
-        setCount(count + 1);
     }
 
     function handleNewTaskChange(event: ChangeEvent<HTMLTextAreaElement>) {
@@ -51,52 +58,88 @@ export function TaskList() {
         event.target.setCustomValidity('Please enter a task');
     }
 
+    const isNewTaskEmpty = newTask.length === 0;
+
+    function handleDeleteTask(id:number) {
+        const receivedTasks = tasks.filter((task) =>{
+            return !(task.id === id);
+        })
+        setTasks(receivedTasks);
+    }
+
+    function handleMarkdownTask(id:number) {
+        tasks.filter(task => task.isComplete === true).length()
+        if(isChecked == true) {
+            const mark = tasks.filter(task => task.isComplete === true).length()
+            }
+            setQtdMarked([...qtdMarked, ]);
+        } else {
+            const receivedTasks = tasks.filter((task) =>{
+                return !(task.id === id);
+            })
+            setQtdMarked(receivedTasks);
+        }
+    }
+
+    //     onChange={() => { handleMarkdownTask(id) }} 
+
     return (
         <div>
             <form onSubmit={handleNewTask}>
                 <div className={styles.newTask}>
-                    <input
-                        // placeholder={tasks} 
+                    <textarea
+                        name="content"
                         placeholder="Adicione uma nova tarefa"
-                        // id={TaskItem.id}
-                        //onChange={handleNewTaskChange}
-                        //onInvalid={handleNewTaskInvalid}
+                        value={newTask}
+                        onChange={handleNewTaskChange}
+                        onInvalid={handleNewTaskInvalid}
                         required
                     >
 
-                    </input>
-                    <button type="submit">Criar <PlusCircle size={17} /></button>
+                    </textarea>
+                    <button type="submit" disabled={isNewTaskEmpty}>Criar <PlusCircle size={17} /></button>
                 </div>
             </form>
             <div className={styles.tasks}>
                 <div className={styles.info}>
                     <div className={styles.created}>
-                        <p>Tarefas Criadas</p>
+                        <p className={styles.pCreated}>Tarefas Criadas</p>
                         <button className={styles.counter}>
-                            <p>0</p>
+                            <p>{tasks.length}</p>
                         </button>
                     </div>
                     <div className={styles.done}>
-                        <p>Concluidas</p>
+                        <p className={styles.pCompleted}>Concluidas</p>
                         <button className={styles.counter}>
-                            <p>0</p>
+                            <p className={styles.pResult}>{qtdMarked.length} de {tasks.length}</p>
                         </button>
                     </div>
                 </div>
-
-                <div className={styles.empty}>
-                    <img src={clipboard}></img>
-                    <h3>Voce ainda nao tem tarefas cadastradas</h3>
-                    <h4>Crie tarefas e organize seus itens a fazer</h4>
-                </div>
+                
                 {/* caso queira utilizar codigo, utilizar chave e caso não for, utilizar parenteses */}
                 {
-                    todos?.map((todo) => (
-                        <Task content={todo.content} isComplete={todo.isComplete} id={todo.id} key={todo.id} />   
-                    ))
+                    tasks?.map(({ id, content, isComplete }) => (
+                        <div className={styles.taskItemList} key={id}>
+                        <input type="checkbox"
+                        checked={isComplete}
+                        onChange={() => {
+                            isComplete = !isComplete;
+                        }} 
+                        
+                        className={styles.taskItemCheck}
+                        />
+                        
+                        
+                        <textarea readOnly value={content} className={styles.taskItemText}/>
+                        <button onClick={() => {handleDeleteTask(id)}} className={styles.taskItemDelete}>
+                            <Trash size={15}/>
+                        </button>
+                    </div>
+                    ),console.log(qtdMarked))
+                        
                 }
-
-
+                
+                
             </div>
         </div>
     )
